f9ecc6ebcd7ad395f83e5b1734abb68a93c96826
rohit <ry4612246@gmail.com>
2024-02-24 22:57:03 +0530

    added

--- app.js
+++ app.js
@@ -154,7 +154,6 @@ app.use((err,req,res,next)=>{
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("App is listening")
-});
-
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
